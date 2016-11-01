6e9fffb7a8c9abfb9bbc566a7f172b418607c87a
Max Barinov <me@maxbarinov.com>
2016-11-02 02:02:31 +0300

    update3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Docker3\n');
+  res.end('Hello World from Docker4\n');
 });
 
 server.listen(port,() => {
