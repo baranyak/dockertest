0663baa4385a6cdeac21f9f0665cd97517e2796a
Baranyak Michael <baranyak.m@gmail.com>
2016-11-04 15:13:45 +0100

    Initial commit for dockercloud branch

--- docker_script.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-cd /usr/share/docker_app/
-sudo docker ps -a -q
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
-sudo docker load -i image_to_deploy
-sudo docker run -d -p 8080:8080 docker_image
