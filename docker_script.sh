0abeee196381c58e48a5361158d252a0e45bb9e8
Baranyak Michael <baranyak.m@gmail.com>
2016-11-04 14:05:44 +0100

    Add AWS instance to configuration scripts

--- docker_script.sh
+++ docker_script.sh
@@ -1,5 +1,8 @@
 #/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker load -i image_to_deploy
-docker run -d -p 8080:8080 image_to_deploy
+cd /usr/share/docker_app/
+ls -al
+sudo docker ps -a -q
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
+sudo docker load -i image_to_deploy
+sudo docker run -d -p 8080:8080 image_to_deploy
