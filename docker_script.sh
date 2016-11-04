61de8ccfb5780fe89e9b13dd16d3c4f060836bbc
Baranyak Michael <baranyak.m@gmail.com>
2016-11-04 14:40:52 +0100

    Changing image name

--- docker_script.sh
+++ docker_script.sh
@@ -1,8 +1,7 @@
 #/bin/bash
 cd /usr/share/docker_app/
 sudo docker ps -a -q
-hostname
 sudo docker stop $(docker ps -a -q)
 sudo docker rm $(docker ps -a -q)
 sudo docker load -i image_to_deploy
-sudo docker run -d -p 8080:8080 image_to_deploy
+sudo docker run -d -p 8080:8080 docker_image
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Docker4\n');
+  res.end('Hello from Docker with Travis, as building engine and repository provider\n');
 });
 
 server.listen(port,() => {
